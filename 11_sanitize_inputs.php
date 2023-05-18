6730507236ccfde647f94574d78652ae7b446900
Nathan Freeman <nathanlfreeman@gmail.com>
2023-05-18 00:10:58 -0400

    resuming tutorial at 11

--- 11_sanitize_inputs.php
+++ 11_sanitize_inputs.php
@@ -1,17 +1,14 @@
 <?php
 
     if(isset($_POST['submit'])) {
-        echo $_POST['name'];
-        echo $_POST['age'];
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+        $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+        echo $name;
     }
 
-    echo $_GET['name'];
-    echo $_GET['age'];
 ?>
 
-<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=Nate&age=41">Click</a>
-
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
     <div>
         <label for="name">Name: </label>
         <input type="text" name="name">
